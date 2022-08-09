6d80a9d31d288551aa25f118c8216ff71e7a4589
ComradeVanti <kingvanti650@gmail.com>
2022-08-09 18:28:16 +0200

    feat: correct I-counts

--- RomanNumeralsPBT.Tests/RomanTests.fs
+++ RomanNumeralsPBT.Tests/RomanTests.fs
@@ -2,6 +2,7 @@
 
 open FsCheck
 open FsCheck.Xunit
+open global.Xunit
 
 type ValidInput =
     static member Default() = Gen.choose (1, 3999) |> Arb.fromGen
@@ -10,11 +11,20 @@ type RomanPropertyAttribute() =
     inherit PropertyAttribute(Arbitrary = [| typeof<ValidInput> |])
 
 
+let private countPatternsForI =
+    [ [ 1; 2; 3; 1; 0 ]
+      [ 2; 3; 1; 0; 1 ]
+      [ 3; 1; 0; 1; 2 ]
+      [ 1; 0; 1; 2; 3 ]
+      [ 0; 1; 2; 3; 1 ] ]
+
 let private validCharacters =
     Set.ofList [ 'I'; 'V'; 'X'; 'L'; 'C'; 'D'; 'M' ]
 
 let private characterIsValid c = validCharacters |> Set.contains c
 
+let private countChar (c: char) (s: string) =
+    s |> Seq.filter ((=) c) |> Seq.length
 
 [<RomanProperty>]
 let ``At least one character`` i = i |> Roman.convert |> String.length >= 1
@@ -22,3 +32,16 @@ let ``At least one character`` i = i |> Roman.convert |> String.length >= 1
 [<RomanProperty>]
 let ``Only valid characters`` i =
     i |> Roman.convert |> Seq.forall characterIsValid
+
+[<Fact>]
+let ``1 is I`` () = Roman.convert 1 = "I"
+
+[<RomanProperty>]
+let ``I follow correct count-pattern`` i =
+
+    let inputs = List.init 5 ((+) i)
+    let converted = inputs |> List.map Roman.convert
+    let counts = converted |> List.map (countChar 'I')
+
+    countPatternsForI |> List.exists ((=) counts)
+    |@ $"%A{counts} matches no count-pattern for I"
--- RomanNumeralsPBT/Roman.fs
+++ RomanNumeralsPBT/Roman.fs
@@ -1,5 +1,10 @@
 ﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
 module RomanNumeralsPBT.Roman
 
-let convert (i: int) = "I"
-
+let convert i =
+    let lastDigit = (i % 10) % 5
+    if lastDigit = 0 then "V"
+    elif lastDigit = 1 then "I"
+    elif lastDigit = 2 then "II"
+    elif lastDigit = 3 then "III"
+    else "I"
