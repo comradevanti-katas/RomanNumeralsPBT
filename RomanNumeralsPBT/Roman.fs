cb462cc542f3062c92f2c7cd9a90e3f4396c9d61
ComradeVanti <kingvanti650@gmail.com>
2022-08-10 20:57:44 +0200

    Revert "feat: correct I-counts"
    This reverts commit 6d80a9d31d288551aa25f118c8216ff71e7a4589.

--- RomanNumeralsPBT/Roman.fs
+++ RomanNumeralsPBT/Roman.fs
@@ -1,10 +1,5 @@
 ﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
 module RomanNumeralsPBT.Roman
 
-let convert i =
-    let lastDigit = (i % 10) % 5
-    if lastDigit = 0 then "V"
-    elif lastDigit = 1 then "I"
-    elif lastDigit = 2 then "II"
-    elif lastDigit = 3 then "III"
-    else "I"
+let convert (i: int) = "I"
+
