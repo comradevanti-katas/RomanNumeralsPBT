336d85ad18e0cd05b40a09eca92aac370a8edbde
ComradeVanti <kingvanti650@gmail.com>
2022-08-12 12:02:50 +0200

    refactor: simplify conversion logic

--- RomanNumeralsPBT/Roman.fs
+++ RomanNumeralsPBT/Roman.fs
@@ -1,27 +1,18 @@
 ﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
 module RomanNumeralsPBT.Roman
 
-let private symbolsWithValues =
-    [ ("I", 1)
-      ("IV", 4)
-      ("V", 5)
-      ("IX", 9)
-      ("X", 10)
-      ("XL", 40)
-      ("L", 50)
-      ("XC", 90)
-      ("C", 100)
-      ("CD", 400)
-      ("D", 500)
-      ("CM", 900)
-      ("M", 1000) ]
-
 let rec convert i =
-    if i > 0 then
-        let symbol, value =
-            symbolsWithValues |> List.findBack (snd >> (>=) i)
-
-        let rest = convert (i - value)
-        symbol + rest
-    else
-        ""
+    if i >= 1000 then "M" + (convert (i - 1000))
+    elif i >= 900 then "CM" + (convert (i - 900))
+    elif i >= 500 then "D" + (convert (i - 500))
+    elif i >= 400 then "CD" + (convert (i - 400))
+    elif i >= 100 then "C" + (convert (i - 100))
+    elif i >= 90 then "XC" + (convert (i - 90))
+    elif i >= 50 then "L" + (convert (i - 50))
+    elif i >= 40 then "XL" + (convert (i - 40))
+    elif i >= 10 then "X" + (convert (i - 10))
+    elif i >= 9 then "IX" + (convert (i - 9))
+    elif i >= 5 then "V" + (convert (i - 5))
+    elif i >= 4 then "IV" + (convert (i - 4))
+    elif i >= 1 then "I" + (convert (i - 1))
+    else ""
